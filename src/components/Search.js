ad9fb426488752385c34d365a3c17625bb9d6919
Borrison <borissoto@gmail.com>
2020-11-12 00:02:13 -0400

    two useEffects for fixing the warning

--- src/components/Search.js
+++ src/components/Search.js
@@ -24,26 +24,13 @@ const Search = () => {
           list: 'search',
           origin: '*',
           format: 'json',
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
       setResults(data.query.search);
     };
-
-    if (term && !results.length) {
-      search();
-    } else {
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          search();
-        }
-      }, 1000);
-
-      return () => {
-        clearTimeout(timeoutId);
-      };
-    }
-  }, [term, results.length]);
+    search();
+  }, [debouncedTerm]);
 
   const renderedResults = results.map((result) => {
     return (
